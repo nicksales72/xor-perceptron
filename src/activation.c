59f1f8b43ca842f0c98f2ba63952118d85e3e96b
Nick <nicksales72@gmail.com>
2025-02-07 00:49:23 -0500

    skill issue

--- src/activation.c
+++ src/activation.c
@@ -5,5 +5,7 @@ double sigmoid(double x) {
 }
 
 double sigmoid_derivative(double x) {
-  return exp(-x) / ((1.0 + exp(-x)) * (1.0 + exp(-x)));
+  double sig = sigmoid(x);
+  return sig * (1 - sig); // apparently more efficient than my derivative done by hand?????? skill issue
+  // return exp(-x) / ((1.0 + exp(-x)) * (1.0 + exp(-x)));
 }
