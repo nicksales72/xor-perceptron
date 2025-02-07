29ceb024012495cb37c5df91e20d484ec9b4d990
Nick <nicksales72@gmail.com>
2025-02-07 01:31:48 -0500

    activation functions before sleep

--- include/activation.h
+++ include/activation.h
@@ -3,7 +3,11 @@
 
 #include <math.h>
 
-double sigmoid(double x);
+double sigmoid_activation(double x);
 double sigmoid_derivative(double x);
+double relu_activation(double x);
+double relu_derivative(double x);
+double tanh_activation(double x);
+double tanh_derivative(double x);
 
 #endif 
--- src/activation.c
+++ src/activation.c
@@ -1,6 +1,6 @@
 #include "../include/activation.h"
 
-double sigmoid(double x) {
+double sigmoid_activation(double x) {
   return 1.0 / (1.0 + exp(-x));
 }
 
@@ -8,4 +8,21 @@ double sigmoid_derivative(double x) {
   double sig = sigmoid(x);
   return sig * (1 - sig); // apparently more efficient than my derivative done by hand?????? skill issue
   // return exp(-x) / ((1.0 + exp(-x)) * (1.0 + exp(-x)));
+}
+
+double relu_activation(double x) {
+  return x > 0 ? x : 0; // x if x > 0, else 0
+}
+
+double relu_derivative(double x) {
+  return x > 0 ? 1 : 0;
+}
+
+double tanh_activation(double x); {
+  return tanh(x);
+}
+
+double tanh_derivative(double x) {
+  double t = tanh(x);
+  return 1 - t * t; // sech^2 = 1 - tanh^2
 }
